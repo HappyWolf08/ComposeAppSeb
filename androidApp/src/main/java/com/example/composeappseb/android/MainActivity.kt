4af329235f2d4317f1b51e1db3585c8ba9e97ec6
Sebastian <sebastian.jamt@gmail.com>
2024-02-09 14:57:41 +0100

    Slutt på VS

--- androidApp/src/main/java/com/example/composeappseb/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/composeappseb/android/MainActivity.kt
@@ -6,29 +6,42 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeappseb.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-
-            ) {
-                Text("Hello")
-                Text("World")
-            }
+                Column (
+                    modifier = Modifier
+                        .background(Color.White)
+                        .fillMaxHeight(0.5f)
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 78.dp)
+                ) {
+                    Text("Hello", modifier = Modifier
+                        .offset(40.dp,50.dp)
+                        .blur(4.dp))
+                    Text("World", modifier = Modifier.rotate(180f))
+                }
         }
     }
 }
